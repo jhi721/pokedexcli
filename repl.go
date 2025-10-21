9d587a31f6a33146f7f064d8d2341f7c6fa9c1a8
Hlib Omelchenko <omelchenkohlib@fastmail.com>
2025-10-21 10:17:23 +0200

    feat(repl.go): add basic repl functionality

--- repl.go
+++ repl.go
@@ -1,7 +1,32 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		scanner.Scan()
+
+		input := cleanInput(scanner.Text())
+
+		if len(input) == 0 {
+			continue
+		}
+
+		command := input[0]
+
+		fmt.Printf("Your command was: %s\n", command)
+	}
 }
